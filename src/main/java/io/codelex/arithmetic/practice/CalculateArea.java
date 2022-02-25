8cd18274089a64b8537b52bf1b75088c1db90114
gunahusko <gunavvv@gmail.com>
2022-02-25 22:47:34 +0200

    arithmetic finished exercise #10

--- src/main/java/io/codelex/arithmetic/practice/CalculateArea.java
+++ src/main/java/io/codelex/arithmetic/practice/CalculateArea.java
@@ -3,7 +3,6 @@ package io.codelex.arithmetic.practice;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-import static java.math.BigDecimal.ZERO;
 
 public class CalculateArea {
     public static void main(String[] args) {
@@ -55,13 +54,12 @@ public class CalculateArea {
 
     public static void calculateCircleArea() {
 
-        BigDecimal radius = ZERO;
+        BigDecimal radius;
 
         // Get input from user
         Scanner keyboard = new Scanner(System.in);
         System.out.print("What is the circle's radius? ");
-        //todo
-        //radius = keyboard.nextDouble();
+        radius = BigDecimal.valueOf(keyboard.nextDouble());
 
         // Display output
         System.out.println("The circle's area is "
@@ -69,46 +67,42 @@ public class CalculateArea {
     }
 
     public static void calculateRectangleArea() {
-        BigDecimal length = ZERO;
-        BigDecimal width = ZERO;
+        BigDecimal length;
+        BigDecimal width;
 
         // Get input from user
         Scanner keyboard = new Scanner(System.in);
 
         // Get length
         System.out.print("Enter length? ");
-        //todo
-        //length = keyboard.nextDouble();
+        length = BigDecimal.valueOf(keyboard.nextDouble());
 
         // Get width
         System.out.print("Enter width? ");
-        //todo
-        //width = keyboard.nextDouble();
+        width = BigDecimal.valueOf(keyboard.nextDouble());
 
         // Display output
         System.out.println("The rectangle's area is "
-                + Geometry.areaOfTriangle(length, width));
+                + Geometry.areaOfRectangle(length, width));
     }
 
     public static void calculateTriangleArea() {
-        BigDecimal base = ZERO;
-        BigDecimal height = ZERO;
+        BigDecimal base;
+        BigDecimal height;
 
         // Get input from user
         Scanner keyboard = new Scanner(System.in);
 
         // Get the base
         System.out.print("Enter length of the triangle's base? ");
-        //todo
-        //base = keyboard.nextDouble();
+        base = BigDecimal.valueOf(keyboard.nextDouble());
 
         // Get the height
         System.out.print("Enter triangle's height? ");
-        //todo
-        //height = keyboard.nextDouble();
+        height = BigDecimal.valueOf(keyboard.nextDouble());
 
         // Display the triangle's area.
         System.out.println("The triangle's area is "
-                + Geometry.areaOfRectangle(base, height));
+                + Geometry.areaOfTriangle(base, height));
     }
 }
--- src/main/java/io/codelex/arithmetic/practice/Geometry.java
+++ src/main/java/io/codelex/arithmetic/practice/Geometry.java
@@ -1,19 +1,46 @@
 package io.codelex.arithmetic.practice;
 
-import io.codelex.NoCodeWrittenException;
+// import io.codelex.NoCodeWrittenException;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 class Geometry {
     static double areaOfCircle(BigDecimal radius) {
-        throw new NoCodeWrittenException();
+
+        if (radius.signum() < 0) {
+            throw new RuntimeException("An error message.");
+        }
+
+        BigDecimal PI = new BigDecimal(Math.PI);
+        BigDecimal areaCircle = radius.multiply(radius).multiply(PI);
+        MathContext m = new MathContext(4);
+        BigDecimal areaCircle2 = areaCircle.round(m);
+        return areaCircle2.doubleValue();
     }
 
     static double areaOfRectangle(BigDecimal length, BigDecimal width) {
-        throw new NoCodeWrittenException();
+
+        if (length.signum() < 0 || width.signum() < 0) {
+            throw new RuntimeException("An error message.");
+        }
+
+        BigDecimal areaRectangle = length.multiply(width);
+        MathContext m = new MathContext(4);
+        BigDecimal areaRectangle2 = areaRectangle.round(m);
+        return areaRectangle2.doubleValue();
     }
 
-    static double areaOfTriangle(BigDecimal base, BigDecimal h) {
-        throw new NoCodeWrittenException();
+    static double areaOfTriangle(BigDecimal base, BigDecimal height) {
+
+        if (base.signum() < 0 || height.signum() < 0) {
+            System.out.println("An error message.");
+        }
+
+        BigDecimal halfOfOne = new BigDecimal("0.50");
+        BigDecimal areaTriangle = halfOfOne.multiply(base).multiply(height);
+        MathContext m = new MathContext(4);
+        BigDecimal areaTriangle2 = areaTriangle.round(m);
+        return areaTriangle2.doubleValue();
     }
 }
