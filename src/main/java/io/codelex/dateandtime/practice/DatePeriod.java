45ecedf88ed71c18cb33f0ecc11f1b2b1eb0da15
gunahusko <gunavvv@gmail.com>
2022-03-14 22:24:37 +0200

    corrected date_and_time

--- src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
+++ src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
@@ -12,35 +12,62 @@ public class DatePeriod {
     }
 
     public DatePeriod intersection(DatePeriod secondDate) {
-        if (startDate.isBefore(secondDate.startDate) && endDate.isAfter(secondDate.startDate)) {
-            return firstDateOverlapTheStartOfSecondDate(secondDate);
+        if ( startDate.isBefore(secondDate.startDate) && endDate.isAfter(secondDate.startDate)) {
+
+            return _1overlapStartOf2 ( secondDate ); // with spaces, because easier to read
+
         } else if (startDate.isAfter(secondDate.startDate) && endDate.isAfter(secondDate.endDate)) {
-            return firstDateOverlapTheEndOfSecondDate(secondDate);
+
+            return _1overlapEndOf2 ( secondDate );
+
         } else if (startDate.isBefore(secondDate.startDate) && endDate.isAfter(secondDate.endDate)) {
-            return secondDate;
+
+            return _1overlapAllOf2 ( secondDate );
+
         } else if (startDate.isAfter(secondDate.startDate) && endDate.isBefore(secondDate.endDate)) {
-            return secondDateOverlapFirstDate(secondDate);
+
+            return _2overlapAllOf1 ();
+
+        } else if (startDate.isBefore(secondDate.startDate) && endDate.isEqual(secondDate.startDate)) {
+
+            return equal1EndWith2Start ( secondDate );
+
+        } else if (startDate.isEqual(secondDate.endDate) && endDate.isAfter(secondDate.endDate)) {
+
+            return equal2EndWith1Start ( secondDate );
+
         } else {
             return null;
         }
     }
 
-    private DatePeriod firstDateOverlapTheStartOfSecondDate(DatePeriod secondDate) {
+    private DatePeriod _1overlapStartOf2(DatePeriod secondDate) {
+        return new DatePeriod(secondDate.startDate, endDate);
+    }
+
+    private DatePeriod _1overlapEndOf2(DatePeriod secondDate) {
+        return new DatePeriod(startDate, secondDate.endDate);
+    }
+
+    private DatePeriod _1overlapAllOf2(DatePeriod secondDate) {
         return new DatePeriod(secondDate.startDate, secondDate.endDate);
     }
 
-    private DatePeriod firstDateOverlapTheEndOfSecondDate(DatePeriod secondDate) {
+    private DatePeriod _2overlapAllOf1() {
         return new DatePeriod(startDate, endDate);
     }
 
-    private DatePeriod secondDateOverlapFirstDate(DatePeriod secondDate) {
-        return new DatePeriod(startDate, endDate);
+    private DatePeriod equal1EndWith2Start(DatePeriod secondDate) {
+        return new DatePeriod(endDate, secondDate.startDate);
     }
 
+    private DatePeriod equal2EndWith1Start(DatePeriod secondDate) {
+        return new DatePeriod(secondDate.endDate, startDate);
+    }
 
     @Override
     public String toString() {
-        return "DatePeriod: startDate = " + startDate +
-                ", endDate = " + endDate;
+        return "start: " + startDate +
+                ", end: " + endDate;
     }
 }
