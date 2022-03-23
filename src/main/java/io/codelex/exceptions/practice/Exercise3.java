b7894f9ef05a4910b01b09d41077d0da7f905a65
gunahusko <gunavvv@gmail.com>
2022-03-23 12:38:49 +0200

    exceptions

--- src/main/java/io/codelex/exceptions/practice/Exercise3.java
+++ src/main/java/io/codelex/exceptions/practice/Exercise3.java
@@ -5,25 +5,18 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Exercise3 {
-//    public static void main(String[] args) throws IOException {
-//        cat(File file);
-//    }
-
-
     public static void cat(File file) throws IOException {
 
-        //TODO - fix this method so it compiles
         RandomAccessFile input = null;
-        String line = null;
+        String line;
 
         try {
             input = new RandomAccessFile(file, "r");
             while ((line = input.readLine()) != null) {
                 System.out.println(line);
             }
-         //   return;
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Something went wrong.");
         } finally {
             if (input != null) {
                 input.close();
