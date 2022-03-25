eb8cc1917fdc4c7d1a1151e1f31b054b13c81ba8
gunahusko <gunavvv@gmail.com>
2022-03-25 12:14:37 +0200

    corrected enum

--- src/main/java/io/codelex/enums/practice/testCardinalPoints.java
+++ src/main/java/io/codelex/enums/practice/testCardinalPoints.java
@@ -4,10 +4,10 @@ public class testCardinalPoints{
     public static void main(String[] args) {
 
         System.out.printf("%-10s %-5s\n", "Direction", "Text");
-        CardinalPoints.printUot(0);
-        CardinalPoints.printUot(1);
-        CardinalPoints.printUot(2);
-        CardinalPoints.printUot(3);
+        CardinalPoints.printUot(CardinalPoints.NORTH.ordinal());
+        CardinalPoints.printUot(CardinalPoints.SOUTH.ordinal());
+        CardinalPoints.printUot(CardinalPoints.EAST.ordinal());
+        CardinalPoints.printUot(CardinalPoints.WEST.ordinal());
         System.out.println();
 
         System.out.println(CardinalPoints.NORTH);
