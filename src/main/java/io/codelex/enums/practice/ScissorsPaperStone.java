45b7b27c9b24af440ff5d5fb7f90522e320c0282
gunahusko <gunavvv@gmail.com>
2022-03-25 16:33:06 +0200

    corrected ScissorsPaperStone

--- src/main/java/io/codelex/enums/practice/ScissorsPaperStone.java
+++ src/main/java/io/codelex/enums/practice/ScissorsPaperStone.java
@@ -1,5 +1,14 @@
 package io.codelex.enums.practice;
 
+import java.util.Random;
+
 public enum ScissorsPaperStone {
-    SCISSORS, STONE, PAPER
+    SCISSORS, STONE, PAPER;
+
+    public static ScissorsPaperStone randomMove() {
+        Random random = new Random();
+        ScissorsPaperStone[] values = ScissorsPaperStone.values();
+        return values[random.nextInt(values.length)];
+    }
+
 }
