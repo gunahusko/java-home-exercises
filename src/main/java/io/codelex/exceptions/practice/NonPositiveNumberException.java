078dd45a6ca9817645611b03df2fc730dae88a27
gunahusko <gunavvv@gmail.com>
2022-04-01 01:38:53 +0300

    corrected exceptions

---
+++ src/main/java/io/codelex/exceptions/practice/NonPositiveNumberException.java
@@ -0,0 +1,8 @@
+package io.codelex.exceptions.practice;
+
+public class NonPositiveNumberException extends Exception{
+
+    NonPositiveNumberException(String message) {
+        super(message);
+    }
+}
